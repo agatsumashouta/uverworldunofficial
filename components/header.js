d22fb647f23d2ad8f4a62e06973fabfae45daa57
agatsumasyouta <0224Syouta>
2023-07-23 00:50:08 +0900

    ああ

--- components/header.js
+++ components/header.js
@@ -22,11 +22,11 @@ const Header = () => {
             <h1 className="rounded-full font-mono font-bold text-7xl">UVERworld非公式ファンサイト</h1>
             <nav>
                 <ul className="flex">
-                    <li><button onClick= {HomeButtonOnclick} className="hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-3">Home</button></li>
-                    <li><button onClick= {MenberButtonOnclick} className="hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Menber</button></li>
-                    <li><button onClick= {MusicButtonOnclick} className="hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Music</button></li>
-                    <li><button onClick= {LinkButtonOnclick} className="hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Link</button></li>
-                    <li><button onClick= {StaffButtonOnclick} className="hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Staff</button></li>
+                    <li><button onClick= {HomeButtonOnclick} className="w-50 h-20 hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-3">Home</button></li>
+                    <li><button onClick= {MenberButtonOnclick} className="w-50 h-20 hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Menber</button></li>
+                    <li><button onClick= {MusicButtonOnclick} className="w-50 h-20 hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Music</button></li>
+                    <li><button onClick= {LinkButtonOnclick} className="w-50 h-20 hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Link</button></li>
+                    <li><button onClick= {StaffButtonOnclick} className="w-50 h-20 hover:bg-gradient-to-b from-blue-500 to-blue-150 pl-1">Staff</button></li>
                 </ul>
             </nav>
         </header>
