29263c77ff875db688e7da54b92a8d490864381f
agatsumasyouta <0224Syouta>
2023-07-22 19:05:17 +0900

    pages作成

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import styles from '../styles/Home.module.css';
 
 function HomePage() {
   return (
-    <div className='styles.container'>
+    <div className={styles.container}>
       <h1>ホームページ</h1>
       <p>ようこそ！</p>
       <Link href="/about">
-        <a>Aboutページへ</a>
+        Aboutページへ
       </Link>
     </div>
   );
